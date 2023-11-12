fd9a5ab73f7d70696723412a670bf668c96f3264
mgonzalez0825 <manuggq@icloud.com>
2023-11-11 21:24:57 -0500

    changing local host to 9090 to test docker backend

--- src/services/jobsService.js
+++ src/services/jobsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";  
 
-const JOBS_API_BASE_URL = 'http://localhost:8080/api/v1/jobs';
+const JOBS_API_BASE_URL = 'http://localhost:9090/api/v1/jobs';
 
 class JobsService {
 
