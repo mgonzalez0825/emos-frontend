30ab770c73632cfa6a101f159f872c2b9dc9c31d
mgonzalez0825 <manuggq@icloud.com>
2023-11-11 21:31:09 -0500

    changed back

--- src/services/departmentService.js
+++ src/services/departmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const DEPT_API_BASE_URL = 'http://localhost:9090/api/v1/departments';
+const DEPT_API_BASE_URL = 'http://localhost:8080/api/v1/departments';
 
 class DepartmentService {
 
